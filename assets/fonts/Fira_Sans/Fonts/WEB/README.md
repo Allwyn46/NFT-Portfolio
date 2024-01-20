f832b7facc32976dc6772ca0fec97194896af6fc
Allwyn46 <allwyn153@gmail.com>
2024-01-20 10:37:55 +0530

    added fonts

---
+++ assets/fonts/Fira_Sans/Fonts/WEB/README.md
@@ -0,0 +1,35 @@
+# Installing Webfonts
+Follow these simple Steps.
+
+## 1.
+Put `fira-sans/` Folder into a Folder called `fonts/`.
+
+## 2.
+Put `fira-sans.css` into your `css/` Folder.
+
+## 3. (Optional)
+You may adapt the `url('path')` in `fira-sans.css` depends on your Website Filesystem.
+
+## 4.
+Import `fira-sans.css` at the top of you main Stylesheet.
+
+```
+@import url('fira-sans.css');
+```
+
+## 5.
+
+
+```
+font-family: 'FiraSans-ExtraLight';
+font-family: 'FiraSans-Light';
+font-family: 'FiraSans-Regular';
+font-family: 'FiraSans-Medium';
+font-family: 'FiraSans-MediumItalic';
+font-family: 'FiraSans-SemiBold';
+font-family: 'FiraSans-SemiBoldItalic';
+font-family: 'FiraSans-BoldItalic';
+font-family: 'FiraSans-ExtraBold';
+font-family: 'FiraSans-Black';
+```
+
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { GiHamburgerMenu } from 'react-icons/gi'
 import '../app/globalS.css'
 import images from '@/constants/images'
 
 const Nav = () => {
+
+    const [toggleMenu, seToggleMenu] = useState(false);
+
     return (
-        <header className='p-5'>
-            <nav className='px-24 flex flex-1 justify-between'>
+        <header className='padding-x py-8 absolute z-10 w-full'>
+            <nav className='px-24 flex justify-between items-center max-container'>
                 <div className='header_logo'>
-                    <img src={images.logo} alt="logo" />
+                    <Image
+                        src={images.logo}
+                        alt="Nft-logo"
+                        style={{
+                            width: '50px',
+                            height: '50px',
+                        }}
+                    />
                 </div>
-                <ul className='flex justify-between'>
-                    <li>Marketplace</li>
+                <ul className='flex items-center header_ul justify-center gap-16 max-lg:hidden'>
+                    <li className='active'>Marketplace</li>
                     <li>Artists</li>
                     <li>Community</li>
                     <li>Collections</li>
                 </ul>
+                <div className='flex items-center'>
+                    <button className='header_but'>Contact us</button>
+                </div>
+
             </nav>
         </header>
     )
