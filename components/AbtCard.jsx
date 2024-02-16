6bc74bcc23ae0d17ee0b728abf87eeb6bf940fc3
Allwyn46 <allwyn153@gmail.com>
2024-02-16 20:39:53 +0530

    minor changes

--- components/AbtCard.jsx
+++ components/AbtCard.jsx
@@ -1,7 +1,6 @@
 "use client"
 import React from 'react'
 import Image from 'next/image'
-import Button from './Button'
 import Counter from './Counter'
 
 const AbtCard = ({ label, imgurl, deadline }) => {
