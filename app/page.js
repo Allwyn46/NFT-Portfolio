08830428dd340d5678d57ac8f951938cabf7c6b1
Allwyn46 <allwyn153@gmail.com>
2024-02-20 20:41:41 +0530

    Added Gallery Section

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Nav from "@/components/Nav";
 import Aboutus from "@/sections/Aboutus";
 import Buysell from "@/sections/Buysell";
+import Gallery from "@/sections/Gallery";
 import Hero from "@/sections/Hero";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Aboutus />
       <Buysell />
+      <Gallery />
     </main>
   );
 }
---
+++ sections/Gallery.jsx
@@ -0,0 +1,22 @@
+"use client"
+import React from 'react'
+
+const Gallery = () => {
+    return (
+        <section className='Gallery_sec w-full max-container    '>
+            <div className='gallery_top_sec flex justify-center items-center'>
+                <h1 className='gallery_top_sec_h1'>
+                    Popular <span>Artists</span> On This Week
+                </h1>
+            </div>
+
+            {/* GALLERY BOTTOM SEC */}
+
+            <div className='gallery_bottom_sec'>
+
+            </div>
+        </section>
+    )
+}
+
+export default Gallery
